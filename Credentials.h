892229f38036d69f0b9906b1f93a96f8a6d08b4f
Leif Percifield <lpercifield@gmail.com>
2012-03-16 16:54:52 -0400

    initial commit

---
+++ Credentials.h
@@ -0,0 +1,15 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// supply your own Pachube feed ID
+#define PACHUBEFEED "50477"
+// this API key will only work from my IP address - you need to supply your own
+#define APIKEY "rewjrtZS0MqOWG4R-j4EJ5u9yPYOvCwKUMuK8O_cwbk"
+#define TIMETOUPDATE 5000  // frequency of update - every 15 seconds
+
+// Wifi parameters
+char passphrase[] = "Zebedee01";
+char ssid[] = "leephMobile";
+boolean mode = WPA_MODE; //or WEP_MODE
+
+#endif
